68592f815e8af4dcf1c1e08e3413fdddb1b9a92f
betich <betich365@gmail.com>
2021-08-22 23:08:50 +0700

    :art: change interface structure to commas

--- src/interfaces/IAuthContext.tsx
+++ src/interfaces/IAuthContext.tsx
@@ -1,9 +1,9 @@
 import { UserData } from "./UserData";
 
 export interface IAuthContext {
-  loading: boolean;
-  signIn: () => void;
-  signOut: () => void;
-  reFetch: () => void;
-  userData: UserData | null;
+  loading: boolean,
+  signIn: () => void,
+  signOut: () => void,
+  reFetch: () => void,
+  userData: UserData | null
 }
