6d31e4b286f417be5ed764e3f8d1b3834942e596
pryter <peterphongpak@gmail.com>
2022-11-17 17:57:29 +0700

    Fix button style

--- src/components.tsx
+++ src/components.tsx
@@ -9,17 +9,16 @@ export const TUCMCLogin = () => {
   const styles = {
     backgroundImage: "linear-gradient(to right, #a78bfa, #ec4899, #ef4444)",
     color: "rgba(255, 255, 255, 1)",
-    padding: "0.5rem 2rem",
-    fontWeight: 500,
+    padding: "0.6rem 2rem",
+    fontWeight: 600,
     borderRadius: "0.375rem",
-    fontSize: "16px",
+    fontSize: "15.4px",
     fontFamily: "Arial, Helvetica, sans-serif",
     lineHeight: "1.25rem",
     border: "none",
     cursor: "pointer",
     transition: "filter 300ms ease",
-    width: "200px",
-    height: "50px"
+    width: "216px",
   }
 
   const hoverStyle = {
